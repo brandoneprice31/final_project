bca63aaca71750d83e91b9deac12dcee96f5e1b3
Stephen Albro <Stephen@Stephens-MacBook-Pro-5.local>
2015-04-17 14:16:28 -0400

    initializer initialized intially with my initiative

--- initializer.py
+++ initializer.py
@@ -5,3 +5,18 @@ Created on Fri Apr 17 13:38:26 2015
 @author: Stephen
 """
 
+########## TYPES ##############
+# type player = X | O
+
+# type action = int (between 1 and 9 inclusive)
+
+# type board = array of arrays
+
+# type state = (board*player)
+
+########## FUNCTIONS ############
+# new(unit): state (empty board and player to move first)
+# next_state(state,action): state
+# eval (state) = player | tie | continue
+# opponent (player) = the other player
+# valid (state,action): bool (is the move legal)
