df7d428e4da97a67901662a1534d7083f3920da1
Brandon Price <brandoneprice31@Brandons-MacBook-Pro-2.local>
2015-04-21 12:59:29 -0400

    created fully-function two player tic tac toe game in graphics and implemented initializer functions

--- graphics.py
+++ graphics.py
@@ -8,22 +8,16 @@ import modules
 """
 from Tkinter import *
 import initializer as init
-#-----------------------------------------------------------------------------
 
+
+#-----------------------------------------------------------------------------
 """
 initialize board, player, switch_player fcn, Window and Canvas
 """
 
-player = 'x'
+player = init.first_player()
 
-# function that changes the player
-def switch_player(p):
-    if (p == 'x'):
-        return 'o'
-    else:
-        return 'x'
-
-board = [['_','_','_'],['_','_','_'],['_','_','_']]
+board = init.new()
 W = Tk()
 
 W.title('Tic Tac Toe')
@@ -34,14 +28,18 @@ cw = 500
 ch = 500
 C = Canvas(W, width=cw, height=ch)
 
-# draw lines
-for i in range(1,3):
-    C.create_rectangle(i*cw/3-1,0,i*cw/3-1,ch, fill="black")
-    
-for i in range(1,3):
-    C.create_rectangle(0,i*cw/3-1,cw,i*cw/3-1, fill="black")          
-#-----------------------------------------------------------------------------
+# draw lines fuction
+def draw_lines ():
+    for i in range(1,3):
+        C.create_rectangle(i*cw/3-1,0,i*cw/3-1,ch, fill="black")
+        
+    for i in range(1,3):
+        C.create_rectangle(0,i*cw/3-1,cw,i*cw/3-1, fill="black")   
 
+draw_lines()
+
+
+#-----------------------------------------------------------------------------
 """
 define draw function
 """
@@ -59,35 +57,85 @@ def draw(b):
                 C.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=xs)
             if (b[i][j] == 'o'):
                 C.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=os)
+
+
 #-----------------------------------------------------------------------------
-
 """
-Add Click Listener
+new_game handler function
 """
 
-# define click_handler for handling click events
-def clicked(event):
-
-    if (0 <= event.y < ch/3):
-        i = 0
-    if (ch/3 <= event.y < ch*2/3):
-        i = 1
-    if (ch*2/3 <= event.y <= ch):
-        i = 2
-    if (0 <= event.x < cw/3):
-        j = 0
-    if (cw/3 <= event.x < cw*2/3):
-        j = 1
-    if (cw*2/3 <= event.x <= cw):
-        j = 2
-    
-    global player    
-    
-    board[i][j] = player
-    player = switch_player(player)
+# function that starts new_game            
+def new_game(event):
+    C.delete('all')
+    global board
+    board = init.new()
+    global player
+    player = init.first_player()
+    draw_lines()
     draw(board)
+    play_game()
 
-C.bind("<Button-1>", clicked)
-C.pack()
+       
+#-----------------------------------------------------------------------------
+"""
+def Play Game Function
+"""
 
-W.mainloop()
+def play_game ():
+    # define click_handler for handling click events
+    def clicked(event):
+    
+        # pinpoint which section was clicked
+        if (0 <= event.y < ch/3):
+            i = 0
+        if (ch/3 <= event.y < ch*2/3):
+            i = 1
+        if (ch*2/3 <= event.y <= ch):
+            i = 2
+        if (0 <= event.x < cw/3):
+            j = 0
+        if (cw/3 <= event.x < cw*2/3):
+            j = 1
+        if (cw*2/3 <= event.x <= cw):
+            j = 2
+        
+        # check if move is valid   
+        global board    
+        if(init.valid(board,(i,j))):
+        
+            # add new move to board
+            global player    
+            board = init.next_state(board,(i,j),player)
+            
+            # draw new board
+            draw(board)    
+            
+            # check game states
+            game_state = init.eval(board,player)
+            
+            # somebody won
+            if (game_state == 'win'):
+                C.create_rectangle(100,100,500-100,500-100, fill="white")
+                if (player=='x'):
+                    message = 'X Wins!'
+                else:
+                    message = 'O Wins!'
+                C.create_text(500/2,500/2,text=message,font=('Purisa',32))
+                C.bind("<Button-1>",new_game)
+                
+            # Cats game
+            if (game_state == 'tie'):
+                C.create_rectangle(100,100,500-100,500-100, fill="white")
+                C.create_text(500/2,500/2,text='Tie Game!',font=('Purisa',32))
+                C.bind("<Button-1>",new_game)
+            
+            # game continues
+            else:   
+                # switch player
+                player = init.opponent(player) 
+    
+    C.bind("<Button-1>", clicked)
+    C.pack()
+    
+    W.mainloop()
+play_game()
--- initializer.py
+++ initializer.py
@@ -15,8 +15,77 @@ Created on Fri Apr 17 13:38:26 2015
 # type state = (board*player)
 
 ########## FUNCTIONS ############
-# new(unit): state (empty board and player to move first)
-# next_state(state,action): state
-# eval (state) = player | tie | continue
-# opponent (player) = the other player
-# valid (state,action): bool (is the move legal)
+#-----------------------------------------------------------------------------
+"""
+new returns an empty board
+"""
+
+def new():
+    return [['_','_','_'],['_','_','_'],['_','_','_']]
+
+
+#-----------------------------------------------------------------------------
+"""
+next_state takes in a state and an action and returns the next state
+"""
+def next_state(state,action,player):
+    i = action[0]
+    j = action[1]
+    state[i][j] = player
+    return state
+    
+
+#-----------------------------------------------------------------------------
+"""
+eval checks if a player won, tied, or continues on a current states
+"""
+def eval (state,pl):
+    # check all possible winning cases
+    if (
+    state[0][0] == state[0][1] == state[0][2] == pl or #across the top
+    state[1][0] == state[1][1] == state[1][2] == pl or #across the middle
+    state[2][0] == state[2][1] == state[2][2] == pl or #across the bottom
+    state[0][0] == state[1][0] == state[2][0] == pl or #down the left
+    state[0][1] == state[1][1] == state[2][1] == pl or #down the middle
+    state[0][2] == state[1][2] == state[2][2] == pl or #down the right
+    state[0][0] == state[1][1] == state[2][2] == pl or #top left diagonal
+    state[2][0] == state[1][1] == state[0][2] == pl):  #bottom left diagonal
+        return 'win'
+    
+    else: 
+        # check if there is '_' which corresponds to continue
+        for i in range(3):
+            for j in range(3):
+                if (state[i][j] == '_'):
+                    return 'continue'
+                    
+        # return tie for cats game
+        return 'tie'
+
+
+#-----------------------------------------------------------------------------
+"""
+opponent takes in the current player and returns the next player
+"""
+def opponent (player):
+    if (player == 'x'):
+        return 'o'
+    else:
+        return 'x'
+
+
+#-----------------------------------------------------------------------------
+"""
+valid takes in a state and action and checks if it is a valid action
+"""
+def valid (state,action):
+    i = action[0]
+    j = action[1]
+    return (state[i][j] == '_')
+
+#-----------------------------------------------------------------------------
+"""
+first_player returns the automatic first player of every game
+"""
+def first_player ():
+    return 'x'
