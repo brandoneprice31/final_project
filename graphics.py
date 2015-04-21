8f44e2cc7ceedb6e093d05e2a485d36d4d2c78ce
Brandon Price <brandoneprice31@Brandons-MacBook-Pro-2.local>
2015-04-20 23:15:56 -0400

    added main code to graphics

--- graphics.py
+++ graphics.py
@@ -1,36 +1,93 @@
 """
-Graphical User Interface Module
+Tic Tac Toe Program
 """
 
-# Gui Interface Module
+
+"""
+import modules
+"""
 from Tkinter import *
-
+import initializer as init
+#-----------------------------------------------------------------------------
 
 """
-    draw takes in a canvas object and board type,
-    and renders the board onto the given canvas using Tkinter GUI
-    """
-def draw (canvas,board):
+initialize board, player, switch_player fcn, Window and Canvas
+"""
+
+player = 'x'
+
+# function that changes the player
+def switch_player(p):
+    if (p == 'x'):
+        return 'o'
+    else:
+        return 'x'
+
+board = [['_','_','_'],['_','_','_'],['_','_','_']]
+W = Tk()
+
+W.title('Tic Tac Toe')
+W.geometry('500x500')
+
+# draw canvas onto window
+cw = 500
+ch = 500
+C = Canvas(W, width=cw, height=ch)
+
+# draw lines
+for i in range(1,3):
+    C.create_rectangle(i*cw/3-1,0,i*cw/3-1,ch, fill="black")
     
-    canvas.delete("all")    
-    
-    ch = 500
-    cw = 500    
-    
-    xs_orig = PhotoImage(file='./ximg.gif')
-    os_orig = PhotoImage(file='./oimg.gif')
-    xs = xs_orig.subsample(2,2)
-    os = os_orig.subsample(2,2)
-    
-    for i in range(1,3):
-        canvas.create_rectangle(i*cw/3-1,0,i*cw/3-1,ch, fill="black")
-    
-    for i in range(1,3):
-        canvas.create_rectangle(0,i*cw/3-1,cw,i*cw/3-1, fill="black")          
-    
+for i in range(1,3):
+    C.create_rectangle(0,i*cw/3-1,cw,i*cw/3-1, fill="black")          
+#-----------------------------------------------------------------------------
+
+"""
+define draw function
+"""
+
+# initialize x and o image
+xs_orig = PhotoImage(file='./ximg.gif')
+os_orig = PhotoImage(file='./oimg.gif')
+xs = xs_orig.subsample(2,2)
+os = os_orig.subsample(2,2)
+
+def draw(b):
     for i in range(3):
         for j in range(3):
-            if (board[i][j] == 'x'):
-                canvas.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=xs)
-            if (board[i][j] == 'o'):
-                canvas.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=os)
+            if (b[i][j] == 'x'):
+                C.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=xs)
+            if (b[i][j] == 'o'):
+                C.create_image(cw*(j+1)/3-cw/6, ch*(i+1)/3-ch/6, image=os)
+#-----------------------------------------------------------------------------
+
+"""
+Add Click Listener
+"""
+
+# define click_handler for handling click events
+def clicked(event):
+
+    if (0 <= event.y < ch/3):
+        i = 0
+    if (ch/3 <= event.y < ch*2/3):
+        i = 1
+    if (ch*2/3 <= event.y <= ch):
+        i = 2
+    if (0 <= event.x < cw/3):
+        j = 0
+    if (cw/3 <= event.x < cw*2/3):
+        j = 1
+    if (cw*2/3 <= event.x <= cw):
+        j = 2
+    
+    global player    
+    
+    board[i][j] = player
+    player = switch_player(player)
+    draw(board)
+
+C.bind("<Button-1>", clicked)
+C.pack()
+
+W.mainloop()
