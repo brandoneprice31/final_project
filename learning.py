3458c7056b126a3438c12e4567bc59b08cc94995
phickman17 <phickman@college.harvard.edu>
2015-04-21 22:02:37 -0400

    peter end of tuesday

--- learning.py
+++ learning.py
@@ -6,39 +6,41 @@ Created on Fri Apr 17 13:39:00 2015
 
 from random import randint
 import Matrices as M
-import Initializer as I
+import initializer as I
 
 all_actions = [(0,0),(0,1),(0,2),(1,0),(1,1),(1,2),(2,0),(2,1),(2,2)]
 
 def reinforcement(board):
     return ""
     
-def chooseMove(state, qTable):
+def chooseMove_random(state, qTable):
   possible_actions = [i for i in all_actions if I.valid(state[0],i) == True]
   size = len(possible_actions)
   return all_actions[randint(0,(size-1))]
 
-"""
-def chooseMove(stateKey, qTable): 
-  actTable = value[stateKey]
-  possible_actions = []
-  List.filter (fun a -> I.valid) actions
-  values = list of values in actTable
-  if player is X
+
+def chooseMove(state, qTable): 
+  stateKey = T.makeKey(state)
+  actTable = qTable.get(stateKey, default=None)
+  possible_actions = [i for i in all_actions if (I.valid(state[0],i) == True)]
+  size = len(possible_actions)
+  values = actTable.values()
+  if (state[1] == 'x'):
      lowest = min(values)
-  else
-     reverse the sign of each value in values list
+  else:
+     values = [i for i in values (-i)]
      lowest = min(values)
-  if lowest < 0
+  if lowest < 0:
      constant = abs(lowest) + 0.1
-     add this constant to each value in values list to make all values positive
-  now perform modified roulette wheel selection using k
+     
+     # add this constant to each value in values list to make all values positive
+     # now perform modified roulette wheel selection using k
   
-  We could also assign probabilities for actions using the formula on page 379
-  of the Mitchell book
-"""
+  # We could also assign probabilities for actions using the formula on page 379
+  # of the Mitchell book
    
-gamma = 0.8 # discount factor 
+# discount factor 
+gamma = 0.8 
 
 def updateQvalue (stateKey, action, nextKey, reward, qTable):
     """"
