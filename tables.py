3e6b9843672c0158996cd68fd50c850959061d73
Stephen Albro <Stephen@Stephens-MacBook-Pro-5.local>
2015-04-23 10:40:44 -0400

    cleaned code a little

--- tables.py
+++ tables.py
@@ -14,9 +14,13 @@ rewardTable = () # rewardTable is mutable dict, how to initialize?
 	# floats [keys in inner actTable] are rewards [0, 1, or -1]; don't update this
 
 def value (board, square, qTable): 
-	# float (* looks up q-value for board and next move square*)
+    # WE will probably come across this function when we implement choosemove
+    # I'll wait until then to implement it.
+    
+    
+    # ((( float (* looks up q-value for board and next move square*)
     # lookup board in qTable, return the actTable
-    # lookup square in actTable, retun q-value
+    # lookup square in actTable, retun q-value )))
 
 def makeKey (state) : 
     string = ""
@@ -32,19 +36,18 @@ def makeKey (state) :
 	# this key is passed into a statetable, say q table
       # The first 9 characters will be the board, the last  characters will be the player
 
-
 def nextKey (state) : 
     makeKey (nextState, opponent(player))
     ## What's up with this function? Not sure we need it.
     ## In main.py, nextKey is a VARIABLE, not a function.
 
 # HELPER FUNCTION: Gets possible actions (ints) from KEYS (string representations of states)
+#   Iterate over the string, saving the index (number) of each '_' that appears.
+#   (Note that we should never get the number 9 as an action, because actions only range
+#   0-8; the ninth charactr in a key always refers to the PLAYER and thus should never be '_'.)
+#   This list of indices is your set of actions.
+#   This function returns a LIST OF INTEGERS.
 def getActions(key):
-    # Iterate over the string, saving the index (number) of each '_' that appears.
-    # (Note that we should never get the number 9 as an action, because actions only range
-    # 0-8; the ninth charactr in a key always refers to the PLAYER and thus should never be '_'.)
-    # thislist is your set of actions.
-    # This function should return a LIST OF INTEGERS
     action_list = []
     list_form = list(key)
     for i in range(len(list_form)):
@@ -53,9 +56,8 @@ def getActions(key):
     return action_list
         
         
-
+# Adds the key to qTable, with its value being a dictionary of actions:Qvals
 def addKey (key, table) : 
-    # Adds the key to qTable, with its value being a dictionary of actions:Qvals
     # Get list of potential actions from the key. This is an int list.
     actions = getActions(key)
     # Store these action as keys in act_dict, values intially randomized on [-0.15,0.15].
