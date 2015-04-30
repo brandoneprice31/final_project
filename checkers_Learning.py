e472e4dba539cda403283e720db751cd87a15633
phickman17 <phickman@college.harvard.edu>
2015-04-30 11:17:19 -0400

    added checkers reinforcement function

--- checkers_Learning.py
+++ checkers_Learning.py
@@ -5,16 +5,19 @@ Created on Sat Apr 25 19:31:55 2015
 @author: brandoneprice31
 """
 
-import checkers_initializer as init
+import checkers_initializer as I
 from random import randint
 
+def reinforcement(state):
+    if I.eval(state['statr'], state['statw']) == 'r_wins':
+            return 1
+    elif I.eval(state['statr'], state['statw']) == 'w_wins':
+            return -1
+    else:
+        return 0
+
 def chooseMove(board,player):
-    pos_act = []
-    for i in range(8):
-        for j in range(8):
-            if (board[i][j] == player+'m' or board[i][j] == player+'k'):
-                for act in init.pos_actions(board,(i,j)):
-                    pos_act.append(act)
+    pos_act = I.allPosMoves(board,player)
     if pos_act != []:
         return pos_act[randint(0,len(pos_act)-1)]
     else:
